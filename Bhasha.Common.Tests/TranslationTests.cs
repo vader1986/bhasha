d9ae422be4ae20ec3406165b948c3144ff3d0b4a
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-02-07 23:16:46 +0000

    Added unit test for Translation class

---
+++ Bhasha.Common.Tests/TranslationTests.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+
+namespace Bhasha.Common.Tests
+{
+    [TestFixture]
+    public class TranslationTests
+    {
+        private static Token AToken = new Token("cat", new string[0], LanguageLevel.A1, TokenType.Noun);
+        private static LanguageToken ALanguageToken = new LanguageToken(Languages.English, "cat", "cat");
+        private static LanguageToken AnotherLanguageToken = new LanguageToken(Languages.Bengoli, "বিড়াল", "Biṛāla");
+
+        [Test]
+        public void Constructor_for_single_language()
+        {
+            Assert.Throws<ArgumentException>(() => new Translation(AToken, ALanguageToken, ALanguageToken));
+        }
+
+        [Test]
+        public void Constructor_for_different_languages()
+        {
+            Assert.DoesNotThrow(() => new Translation(AToken, ALanguageToken, AnotherLanguageToken));
+        }
+    }
+}
