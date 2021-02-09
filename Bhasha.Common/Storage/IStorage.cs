cf489957420a2b8689d50402f125ccd88486a814
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-02-09 14:52:54 +0000

    Removed useless interface method

--- Bhasha.Common/Storage/IStorage.cs
+++ Bhasha.Common/Storage/IStorage.cs
@@ -6,6 +6,5 @@ namespace Bhasha.Common.Storage
     public interface IStorage
     {
         IEnumerable<Translation> Query(QueryParams query);
-        IEnumerable<Translation> Query(string id);
     }
 }
