e3230acd2a445c509cb35ebf7fe615db128fda7c
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-02-21 22:17:01 +0000

    Increased unit test coverage for ProcedureDto

--- Bhasha.Common.MongoDB/Dto/ProcedureDto.cs
+++ Bhasha.Common.MongoDB/Dto/ProcedureDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using Bhasha.Common.Extensions;
+using Bhasha.Common.MongoDB.Exceptions;
 
 namespace Bhasha.Common.MongoDB.Dto
 {
@@ -13,12 +15,19 @@ namespace Bhasha.Common.MongoDB.Dto
 
         public Procedure ToProcedure()
         {
-            var id = new ProcedureId(ProcedureId);
-            var tutorial = Tutorial.Select(x => new ResourceId(x)).ToArray();
-            var support = Support.Select(Enum.Parse<TokenType>).ToArray();
-            var audio = ResourceId.Create(AudioId);
+            try
+            {
+                var id = new ProcedureId(ProcedureId);
+                var tutorial = Tutorial != default ? Tutorial.Select(x => new ResourceId(x)).ToArray() : default;
+                var support = Support.Select(Enum.Parse<TokenType>).ToArray();
+                var audio = ResourceId.Create(AudioId);
 
-            return new Procedure(id, Description, tutorial, audio, support);
+                return new Procedure(id, Description, tutorial, audio, support);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidProcedureException($"loaded invalid procedure {this.Stringify()}", e);
+            }
         }
     }
 }
---
+++ Bhasha.Common.MongoDB/Exceptions/InvalidProcedureException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Bhasha.Common.MongoDB.Exceptions
+{
+    public class InvalidProcedureException : Exception
+    {
+        public InvalidProcedureException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
