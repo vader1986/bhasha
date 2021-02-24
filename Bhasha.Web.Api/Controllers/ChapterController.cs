09403023ab0824fb6447651486eaf0452de73fe3
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-02-24 19:20:53 +0000

    Added VS project for the Web API

---
+++ Bhasha.Web.Api/Controllers/ChapterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Bhasha.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bhasha.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("api/chapter")]
+    public class ChapterController : Controller
+    {
+        [HttpGet("next/{userId}")]
+        public async Task<ActionResult<Chapter>> GetNext(string userId)
+        {
+            return Ok(null);
+        }
+    }
+}
