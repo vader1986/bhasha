62d143807976a52ea9d7ad28d69858c3eba082ab
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-03-21 23:21:34 +0000

    Fixed UI unit test

--- Bhasha.Web.Client/src/App.test.js
+++ Bhasha.Web.Client/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Primary Button/i);
+  const linkElement = screen.getByText(/Create Profile/i);
   expect(linkElement).toBeInTheDocument();
 });
