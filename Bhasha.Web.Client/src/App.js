9116b8fec1890b392c662af5c4004ae8a8c476c1
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-03-21 20:58:20 +0000

    Added collapse for create profile UI

--- Bhasha.Web.Client/src/App.js
+++ Bhasha.Web.Client/src/App.js
@@ -21,11 +21,28 @@ const fakeProfile = {
   completedChapters: 0
 };
 
+const fakeProfile2 = {
+  id: "d494e433-ee72-48e8-a45a-3f0eed6a0d34",
+  userId: "test",
+  from: {
+    id: "bn",
+    name: "Bengali",
+    region: null
+  },
+  to: {
+    id: "en",
+    name: "English",
+    region: "UK"
+  },
+  level: 1,
+  completedChapters: 0
+};
+
 function App() {
   return (
     <Container maxWidth="sm" className="App">
       <Paper>
-        <ProfileList profiles={[fakeProfile]} />
+        <ProfileList profiles={[fakeProfile, fakeProfile2]} />
       </Paper>
     </Container>
   );
--- Bhasha.Web.Client/src/features/ProfileList.js
+++ Bhasha.Web.Client/src/features/ProfileList.js
@@ -2,14 +2,28 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { Collapse, Paper } from '@material-ui/core';
 
 function ProfileList(props) {
+    const [open, setOpen] = React.useState(true);
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (
         <div>
             <List component="nav">
-                <ListItem button key="create">
+                <ListItem button key="create" onClick={handleClick}>
                     <ListItemText>Create Profile ...</ListItemText>
                 </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Paper>
+                        asdasds
+                        asd
+                        a
+                        sdasd
+                    </Paper>
+                </Collapse>
                 {props.profiles.map(profile => 
                 <ListItem button key={profile.id}>
                     <ListItemText style={{color: '#005FFF'}}>
