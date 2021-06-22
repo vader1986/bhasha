ee2dd93eae6251cd73a4f731dd4d7ba9e8190fa0
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-06-22 18:12:01 +0100

    Integrated Student API into Blazor Web App

--- Bhasha.Student.Web/Services/StudentApiClient.cs
+++ Bhasha.Student.Web/Services/StudentApiClient.cs
@@ -2,19 +2,26 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Bhasha.Common;
+using Bhasha.Common.Extensions;
 
 namespace Bhasha.Student.Web.Services
 {
     public interface IStudentApiClient
     {
+        Task<ChapterEnvelope[]> ListChapters(Guid profileId, int level = int.MaxValue);
+        Task<Stats> GetStats(Guid profileId, Guid chapterId);
+        Task<Evaluation> SubmitPage(Guid profileId, Guid chapterId, int pageIndex, string solution);
+        Task<string> RequestTip(Guid profileId, Guid chapterId, int pageIndex);
         Task<Profile> CreateProfile(string native, string target);
+        Task<Profile[]> ListProfiles();
         Task<Language[]> GetLanguages();
+        Task DeleteProfile(Guid profileId);
+        Task DeleteUser();
     }
 
     public class StudentApiClient : IStudentApiClient
     {
         private readonly ISettingsProvider _settingsProvider;
-        private HttpClient _client;
 
         public StudentApiClient(ISettingsProvider settingsProvider)
         {
@@ -23,51 +30,74 @@ namespace Bhasha.Student.Web.Services
 
         private async Task<HttpClient> GetHttpClient()
         {
-            var uri = await _settingsProvider.GetSettings();
-            return _client ??= new HttpClient
+            return new HttpClient
             {
-                BaseAddress = new Uri(uri.StudentApi),
-                Timeout = TimeSpan.FromSeconds(5),                
+                BaseAddress = new Uri((await _settingsProvider.GetSettings()).StudentApi),
+                Timeout = TimeSpan.FromSeconds(5)
             };
         }
 
-        public async Task<Profile> CreateProfile(string native, string target)
+        private async Task<T> Post<T>(string url, HttpContent content = default)
         {
-            var url = $"api/profile/create?native={native}&target={target}";
-
-            var client = await GetHttpClient();
-            var response = await client.PostAsync(url, default);
-
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsAsync<Profile>();
+            using var client = await GetHttpClient();
+            return await client.PostAsync<T>(url, content);
         }
 
-        public async Task<Language[]> GetLanguages()
+        private async Task<T> Get<T>(string url)
         {
-            var url = "api/profile/languages";
+            using var client = await GetHttpClient();
+            return await client.GetAsync<T>(url);
+        }
 
-            var client = await GetHttpClient();
-            Console.WriteLine("GetAsync: " + client.BaseAddress.ToString() + " " + url);
+        private async Task Delete(string url)
+        {
+            using var client = await GetHttpClient();
+            await client.DeleteAsync(url);
+        }
 
-            try
-            {
-                var response = await client.GetAsync(url);
+        public Task<ChapterEnvelope[]> ListChapters(Guid profileId, int level = int.MaxValue)
+        {
+            return Get<ChapterEnvelope[]>($"api/chapter/list?profileId={profileId}&level={level}");
+        }
 
-                Console.WriteLine("GetAsync DONE");
+        public Task<Stats> GetStats(Guid profileId, Guid chapterId)
+        {
+            return Get<Stats>($"api/chapter/stats?profileId={profileId}&chapterId={chapterId}");
+        }
 
-                response.EnsureSuccessStatusCode();
+        public Task<Evaluation> SubmitPage(Guid profileId, Guid chapterId, int pageIndex, string solution)
+        {
+            return Post<Evaluation>($"api/page/submit?profileId={profileId}&chapterId={chapterId}&pageIndex={pageIndex}&solution={solution}");
+        }
 
-                Console.WriteLine("OK");
+        public Task<string> RequestTip(Guid profileId, Guid chapterId, int pageIndex)
+        {
+            return Post<string>($"api/page/tip?profileId={profileId}&chapterId={chapterId}&pageIndex={pageIndex}");
+        }
 
-                return await response.Content.ReadAsAsync<Language[]>();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("error: " + e.Message);
-                Console.WriteLine("error: " + e.StackTrace);
-                throw;
-            }
+        public Task<Profile> CreateProfile(string native, string target)
+        {
+            return Post<Profile>($"api/profile/create?native={native}&target={target}");
+        }
+
+        public Task<Profile[]> ListProfiles()
+        {
+            return Get<Profile[]>("api/profile/list");
+        }
+
+        public Task<Language[]> GetLanguages()
+        {
+            return Get<Language[]>("api/profile/languages");
+        }
+
+        public Task DeleteProfile(Guid profileId)
+        {
+            return Delete("api/profile/delete");
+        }
+
+        public Task DeleteUser()
+        {
+            return Delete("api/user/delete");
         }
     }
 }
