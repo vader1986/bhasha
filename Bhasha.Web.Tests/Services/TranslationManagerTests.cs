c9789b2c8515efb870fa170f3b861601645a24f0
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2022-02-26 16:09:37 +0000

    Added missing unit test

--- Bhasha.Web.Tests/Services/TranslationManagerTests.cs
+++ Bhasha.Web.Tests/Services/TranslationManagerTests.cs
@@ -1,7 +1,39 @@
-﻿namespace Bhasha.Web.Tests.Services
+﻿using System;
+using AutoFixture.Xunit2;
+using Bhasha.Web.Domain;
+using Bhasha.Web.Interfaces;
+using Bhasha.Web.Services;
+using NSubstitute;
+using Xunit;
+
+namespace Bhasha.Web.Tests.Services
 {
 	public class TranslationManagerTests
 	{
+		private readonly TranslationManager _translationManager;
+		private readonly IRepository<Translation> _translations;
+		private readonly IRepository<Expression> _expressions;
+		private readonly IFactory<Expression> _expressionFactory;
+
+		public TranslationManagerTests()
+        {
+			_expressions = Substitute.For<IRepository<Expression>>();
+			_expressionFactory = Substitute.For<IFactory<Expression>>();
+			_translations = Substitute.For<IRepository<Translation>>();
+			_translationManager = new TranslationManager(_translations, _expressions, _expressionFactory);
+		}
+
+		[Theory, AutoData]
+		public void GivenNonReferenceTranslation_WhenAddOrUpdateWithoutReference_ThenThrowException(Translation translation)
+        {
+			// setup
+			var translationNoRef = translation with { Language = Language.Bengali };
+
+			// act & verify
+			Assert.ThrowsAsync<ArgumentException>(async () =>
+				await _translationManager.AddOrUpdate(translation, default));
+        }
+
 		/*
 		 * ToDo:
 		 * - unit tests for AddOrUpdate method
