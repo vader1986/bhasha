3e697dfbfddf7a98fcd9b31b5c1e54350b7c055e
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2024-03-08 12:15:41 +0000

    added audio support

--- Bhasha/Web/Shared/Components/MultipleChoicePage.razor.cs
+++ Bhasha/Web/Shared/Components/MultipleChoicePage.razor.cs
@@ -1,4 +1,5 @@
-﻿using Bhasha.Domain;
+﻿using System.Speech.Synthesis;
+using Bhasha.Domain;
 using Bhasha.Domain.Pages;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
@@ -10,8 +11,11 @@ public partial class MultipleChoicePage : ComponentBase
     [Parameter] public required DisplayedPage<MultipleChoice> Data { get; set; }
     [Parameter] public required Func<Translation, Task> Submit { get; set; }
 
+    private bool DisableAudio => _selectedChoice == null ||
+                                 _selectedChoice.Value is Translation translation && string.IsNullOrWhiteSpace(translation.Spoken) ||
+                                 Environment.OSVersion.Platform == PlatformID.MacOSX;
     private bool DisableSubmit => _selectedChoice == null;
-
+    
     private MultipleChoice? _arguments;
     private MudChip? _selectedChoice;
 
@@ -23,7 +27,25 @@ public partial class MultipleChoicePage : ComponentBase
         _arguments = Data.Arguments;
     }
 
-    internal void OnSubmit()
+    private void OnPlayAudioClicked()
+    {
+        if (_selectedChoice == null)
+            return;
+
+        var translation = (Translation)_selectedChoice.Value;
+        
+        if (string.IsNullOrWhiteSpace(translation.Spoken))
+            return;
+
+        if (Environment.OSVersion.Platform == PlatformID.MacOSX)
+            return;
+        
+        using var synthesizer = new SpeechSynthesizer();
+        synthesizer.SetOutputToDefaultAudioDevice();
+        synthesizer.SpeakAsync(translation.Spoken);
+    }
+
+    private void OnSubmit()
     {
         if (_selectedChoice == null)
             return;
