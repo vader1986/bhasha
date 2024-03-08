34fda48db68eccccac7594fc0a3360a33ea82cb3
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2024-03-08 13:22:54 +0000

    minor improvement

--- Bhasha/Web/Shared/Components/ChapterPage.razor.cs
+++ Bhasha/Web/Shared/Components/ChapterPage.razor.cs
@@ -7,8 +7,20 @@ public partial class ChapterPage : ComponentBase
 {
     [Parameter] public required DisplayedChapter Chapter { get; set; }
     [Parameter] public required DisplayedPage Page { get; set; }
+    [Parameter] public required Bhasha.Domain.ChapterSelection Selection { get; set; }
     [Parameter] public required Func<Translation, Task> Submit { get; set; }
 
+    private int ChapterProgress
+    {
+        get
+        {
+            var totalPages = Chapter.Pages.Length;
+            var correctResults = Selection.Pages.Count(x => x == ValidationResult.Correct);
+            
+            return (int)Math.Round(100 * (double)correctResults / totalPages);
+        }
+    }
+    
     internal DisplayedPage<T> GetPage<T>()
     {
         return (DisplayedPage<T>)Page;
