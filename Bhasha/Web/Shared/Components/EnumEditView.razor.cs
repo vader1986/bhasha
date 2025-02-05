fb3162a989e6477788287ae6d1dcb2177f7e7a9a
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2025-02-05 22:28:24 +0000

    try

--- Bhasha/Web/Shared/Components/EnumEditView.razor.cs
+++ Bhasha/Web/Shared/Components/EnumEditView.razor.cs
@@ -4,31 +4,31 @@ namespace Bhasha.Web.Shared.Components;
 
 public partial class EnumEditView<T> : ComponentBase where T : struct, Enum
 {
+    private const string None = "None";
     private static string Label => typeof(T).Name;
-    private static string[] Values => Enum.GetNames<T>();
+    private static string[] Values => Enum.GetNames<T>().Append(None).ToArray();
 
-    [Parameter] public required T Value { get; set; }
-    [Parameter] public EventCallback<T> ValueChanged { get; set; }
+    [Parameter] public required T? Value { get; set; }
+    [Parameter] public EventCallback<T?> ValueChanged { get; set; }
 
-    private string _selectedValue = "";
+    private string _selectedValue = None;
     
     protected override void OnParametersSet()
     {
-        _selectedValue = Value.ToString();
+        _selectedValue = Value?.ToString() ?? None;
 
         base.OnParametersSet();
     }
 
-    private async Task OnValueChanged(string value)
+    private async Task OnValueChanged()
     {
-        if (_selectedValue == value)
-            return;
-        
-        _selectedValue = value;
-        
-        if (Enum.TryParse<T>(value, out var actualValue))
+        if (Enum.TryParse<T>(_selectedValue, out var actualValue))
         {
             await ValueChanged.InvokeAsync(actualValue);
+        }
+        else
+        {
+            await ValueChanged.InvokeAsync(null);
         }
     }
 }
