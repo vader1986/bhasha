1f0a4d6a99f6937f358e3da3eb3a993367b10817
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2025-03-03 16:00:59 +0000

    try fix

--- Bhasha/Web/Pages/Login.razor.cs
+++ Bhasha/Web/Pages/Login.razor.cs
@@ -1,12 +1,15 @@
 using Bhasha.Identity;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
+using MudBlazor;
 
 namespace Bhasha.Web.Pages;
 
 public partial class Login : ComponentBase
 {
+    [Inject] public required ISnackbar Snackbar { get; set; }
     [Inject] public required SignInManager<AppUser> SignInManager { get; set; }
+    [Inject] public required NavigationManager NavigationManager { get; set; }
     
     private readonly LoginModel _model = new()
     {
@@ -16,12 +19,28 @@ public partial class Login : ComponentBase
 
     private async Task LoginAsync()
     {
-        await SignInManager
-            .PasswordSignInAsync(
-                userName: _model.Username, 
-                password: _model.Password, 
-                isPersistent: true, 
-                lockoutOnFailure: false);
+        var result = await SignInManager.CheckPasswordSignInAsync(new AppUser
+        {
+            UserName = _model.Username
+        }, _model.Password, false);
+
+        if (result.Succeeded)
+        {
+            await SignInManager
+                .PasswordSignInAsync(
+                    userName: _model.Username, 
+                    password: _model.Password, 
+                    isPersistent: true, 
+                    lockoutOnFailure: false);
+            
+            Snackbar.Add("Successful login!", Severity.Success);
+
+            NavigationManager.NavigateTo("/");
+        }
+        else
+        {
+            Snackbar.Add("Invalid username or password!", Severity.Error);
+        }
     }
     
     private class LoginModel
